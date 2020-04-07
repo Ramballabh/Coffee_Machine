9c006907c056bd18d24ae85c9a1cb16dd632d614
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-04-07 13:47:50 +0530

    give definition to method by writing statements.

--- src/stage1/Stage1.java
+++ src/stage1/Stage1.java
@@ -2,6 +2,12 @@ package stage1;
 
 public class Stage1 {
     public void printProcessSteps() {
-
+        System.out.println("Starting to make a coffee");
+        System.out.println("Grinding coffee beans");
+        System.out.println("Boiling water");
+        System.out.println("Mixing boiled water with crushed coffee beans");
+        System.out.println("Pouring coffee into the cup");
+        System.out.println("Pouring some milk into the cup");
+        System.out.println("Coffee is ready!");
     }
 }
