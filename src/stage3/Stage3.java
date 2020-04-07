95a7677cfc25be083d5ba8ea69eb745bd3a1cc77
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-04-07 16:05:46 +0530

    print the message using if else

--- src/stage3/Stage3.java
+++ src/stage3/Stage3.java
@@ -11,6 +11,13 @@ public class Stage3 {
     }
 
     public void printMessage(int n, int min) {
-
+        if (n == min) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (n < min) {
+            System.out.println("Yes, I can make that amount of coffee (and even " +
+                    (min - n) + " more than that)");
+        } else {
+            System.out.println("No, I can make only " + min + " cup(s) of coffee");
+        }
     }
 }
