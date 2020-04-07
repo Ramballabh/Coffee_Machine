d6362508dc96c49f2debe136708052854e767b7a
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-04-07 16:02:45 +0530

    calculate amount of different substance

--- src/stage3/Stage3.java
+++ src/stage3/Stage3.java
@@ -2,7 +2,9 @@ package stage3;
 
 public class Stage3 {
     public void amountOfCoffee(int water, int milk, int beans, int n) {
-
+        water /= 200;
+        milk /= 50;
+        beans /= 15;
 
     }
 }
