e7107208460e207d2c63c065b404063d63b03d39
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-04-07 14:08:15 +0530

    take input as number of coffees.

--- src/main/Main.java
+++ src/main/Main.java
@@ -17,6 +17,7 @@ public class Main {
             case 2:
                 Stage2 stage2 = new Stage2();
                 System.out.println("Write how many cups of coffee you will need:");
+                int coffees = scanner.nextInt();
         }
     }
 }
