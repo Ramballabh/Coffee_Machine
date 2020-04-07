09e654851c44878ae2666ebdc724df830f31c648
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-04-07 16:02:15 +0530

    create new parameterized method

--- src/main/Main.java
+++ src/main/Main.java
@@ -26,10 +26,13 @@ public class Main {
 
                 System.out.println("Write how many ml of water the coffee machine has:");
                 int water = scanner.nextInt();
+
                 System.out.println("Write how many ml of milk the coffee machine has:");
                 int milk = scanner.nextInt();
+
                 System.out.println("Write how many grams of coffee beans the coffee machine has:");
                 int beans = scanner.nextInt();
+
                 System.out.println("Write how many cups of coffee you will need:");
                 int n = scanner.nextInt();
         }
--- src/stage3/Stage3.java
+++ src/stage3/Stage3.java
@@ -1,5 +1,8 @@
 package stage3;
 
 public class Stage3 {
+    public void amountOfCoffee(int water, int milk, int beans, int n) {
 
+
+    }
 }
