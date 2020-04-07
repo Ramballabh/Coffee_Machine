abec687518f38ebd8b30c9144dd7cfd90287e38e
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-04-07 15:46:37 +0530

    print the message

--- src/stage2/Stage2.java
+++ src/stage2/Stage2.java
@@ -5,6 +5,10 @@ public class Stage2 {
         int water = 200 * coffee;
         int milk = 50 * coffee;
         int beans = 15 * coffee;
+        System.out.println("For " + coffee + " cups of coffee you will need:");
+        System.out.println(water + " ml of water");
+        System.out.println(milk + " ml of milk");
+        System.out.println(beans + " g of coffee beans");
     }
 
 }
