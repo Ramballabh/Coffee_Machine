89af0247335b4fc86606ee95dd3a74b6427d0cd6
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-04-07 15:45:51 +0530

    calculate different amount of material.

--- src/stage2/Stage2.java
+++ src/stage2/Stage2.java
@@ -2,7 +2,9 @@ package stage2;
 
 public class Stage2 {
     public void quantityOfDifferentMaterial(int coffee) {
-
+        int water = 200 * coffee;
+        int milk = 50 * coffee;
+        int beans = 15 * coffee;
     }
 
 }
