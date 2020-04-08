b58171e3e2878ed3143dcb467847e855f70669d5
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-04-08 09:46:49 +0530

    create object of Scanner class.

--- src/stage5/CoffeeMachine.java
+++ src/stage5/CoffeeMachine.java
@@ -1,9 +1,13 @@
 package stage5;
 
+import java.util.Scanner;
+
 public class CoffeeMachine {
     static int water = 400;
     static int milk = 540;
     static int coffeeBeans = 120;
     static int disposableCups = 9;
     static int money = 550;
+
+    static Scanner in = new Scanner(System.in);
 }
