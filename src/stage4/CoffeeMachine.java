b7904263f454ee6a9fdd22bf8a1bfe73d178fe1d
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-04-08 08:32:24 +0530

    define new fields in CoffeeMachine.java

--- src/stage4/CoffeeMachine.java
+++ src/stage4/CoffeeMachine.java
@@ -4,5 +4,10 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
     Scanner scanner = new Scanner(System.in);
+    int cups;
+    int waterN;
+    int milkN;
+    int coffeeN;
+    int money;
 
 }
