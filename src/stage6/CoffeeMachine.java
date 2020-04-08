3dcd6806e086daf20adae497d27f41a9a200b3c7
Ram ballabh <ramballabh.agrawal_cs18@gla.ac.in>
2020-04-08 10:25:27 +0530

    define private fields

--- src/stage6/CoffeeMachine.java
+++ src/stage6/CoffeeMachine.java
@@ -3,5 +3,12 @@ package stage6;
 import java.util.Scanner;
 
 public class CoffeeMachine {
+
     private Scanner scanner;
+
+    private int water;
+    private int milk;
+    private int beans;
+    private int cups;
+    private int money;
 }
